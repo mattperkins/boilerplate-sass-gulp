80794aea4972036b6778a357907208ee489ba224
mattperkins <github@mattperkins.com>
2017-12-05 17:52:20 +0000

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function showSize() {
+    var w = window.innerWidth;
+    document.getElementById('size').innerHTML = "W: " + w;
+}
+showSize();
+
+// (function(){
+
+//   window.console.log('hello world!');
+
+// }());
+
+
+// JQUERY
+
+$(document).ready(function(){
+
+  // code
+
+});
+
